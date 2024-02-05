ec7fdd46816bcfbb08ee039200b2a3fe5d6283b3
Mathias <email@example.com>
2024-02-05 15:46:21 +0100

    add DIR

--- scripts/test_bash_script_starter.sh
+++ scripts/test_bash_script_starter.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 SCRIPT_NAME=$(basename "${0}")
-SCRIPT="$0"
-SCRIPT_PATH="$(dirname "$SCRIPT")"
+SCRIPT_NAME_W_FULL_PATH=$(readlink -f $0)
+SCRIPT_PATH="$(dirname "$SCRIPT_NAME")"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo "Hello , I'm a bash script STARTER!"
+echo "  "
 echo "Script Name => $SCRIPT_NAME"
 echo "Script Path => $SCRIPT_PATH "
+echo "Script FULL Path => $SCRIPT_NAME_W_FULL_PATH"
+echo "Directory of Script => $DIR"
