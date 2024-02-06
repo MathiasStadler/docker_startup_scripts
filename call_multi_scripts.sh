2e4374234c15704eaf1d6e68cc09c08ed8eb93fe
Mathias <email@example.com>
2024-02-06 11:38:09 +0100

    work with error

--- call_multi_scripts.sh
+++ call_multi_scripts.sh
@@ -4,7 +4,7 @@ SCRIPT_FOLDER="./scripts"
 for SCRIPT in $(ls "$SCRIPT_FOLDER" | sort -g)
 do
 out=$($SCRIPT_FOLDER/$SCRIPT 2>$?); rc=$?;
-echo "RUN SCRIPT $SCRIPT WITH EXIT CODE $rc"
+echo "RUN SCRIPT $SCRIPT EXIT-CODE => $rc"
 if  [ "$rc" -ne "0" ]; then
     echo "error $rc in script $SCRIPT: $out" >&2
     exit $rc
